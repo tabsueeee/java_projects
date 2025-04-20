2f03be28bf6610c97d34fa2af61dbd08b8252147
Cristian <tabuaccristianjay@gmail.com>
2025-04-21 00:20:34 +0800

    @fix: Adjust the codes

--- MD5Hashing.java
+++ MD5Hashing.java
@@ -16,17 +16,16 @@ public class MD5Hashing {
             byte[] digest = md.digest();
             StringBuilder hexString = new StringBuilder();
             for (byte b : digest) {
+                // Convert each byte to a hexadecimal string
                 String hex = Integer.toHexString(0xFF & b);
                 if (hex.length() == 1) {
                     hexString.append('0');
                 }
                 hexString.append(hex);
             }
-            // Print the MD5 hash
+            // Print output
             System.out.println("MD5 hash: " + hexString.toString());
-            // Print the length of the hash
             System.out.println("Length of the hash: " + hexString.length());
-            // Print the hash in hexadecimal format
             System.out.print("Hash in hexadecimal format: ");
             for (byte b : digest) {
                 String hex = Integer.toHexString(0xFF & b);
@@ -37,14 +36,9 @@ public class MD5Hashing {
                 System.out.print(hex);
             }
 
-
-
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
-
-
-
 
 }
